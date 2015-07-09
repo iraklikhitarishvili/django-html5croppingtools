b9e85cb59d93a4dae38a70ba559135d7a39633ff
irakli <iraklikhitarishvili@yahoo.com>
2015-07-09 17:29:04 +0400

    license

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-html5croppingtools',
+    version='0.1',
+    packages=['html5croppingtools'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to crop and resize images',
+    long_description=README,
+    url='http://www.example.com/',
+    author='irakli khitarishvili',
+    author_email='',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
