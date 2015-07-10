8873a018e6c3a025f8fd5c9a33f1ed90d2694605
irakli <iraklikhitarishvili@yahoo.com>
2015-07-11 02:14:10 +0400

    rename

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-html5croppingtools',
-    version='0.1',
+    version='0.1.1',
     packages=['html5croppingtools'],
     include_package_data=True,
     license='BSD License',  # example license
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
